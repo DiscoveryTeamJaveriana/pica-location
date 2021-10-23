d440133fdb0db55731117597ceab9b88d80847c1
DiegoABorrero <diego.borrero@payu.com>
2021-10-23 17:41:09 -0500

    First Commit

---
+++ src/main/java/co/edu/javeriana/discovery/pica/location/PicaLocationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicaLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicaLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/discovery/pica/location/PicaLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicaLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
